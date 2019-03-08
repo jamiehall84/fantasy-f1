629c6aed3a7cfeb8fa815bb52eaefad7c3323cfb
Jamie Hall <jamie@creativedigitaldesign.net>
2019-03-08 00:05:47 +0000

    Fixed formatting

--- src/App.js
+++ src/App.js
@@ -10,24 +10,16 @@ import app from './base'
 require('dotenv').config()
 
 const Page = ({ title }) => (
-    <div className="App">
-      <div className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <h2>{title}</h2>
-      </div>
-      <p className="App-intro">
-        This is the {title} page.
-      </p>
-      <p>
-        <Link to="/">Home</Link>
-      </p>
-      <p>
-        <Link to="/about">About</Link>
-      </p>
-      <p>
-        <Link to="/settings">Settings</Link>
-      </p>
+  <div className="App">
+    <div className="App-header">
+      <img src={logo} className="App-logo" alt="logo" />
+      <h2>{title}</h2>
     </div>
+    <p className="App-intro">This is the {title} page.</p>
+    <p><Link to="/">Home</Link></p>
+    <p><Link to="/about">About</Link></p>
+    <p><Link to="/settings">Settings</Link></p>
+  </div>
 );
 
 const About = (props) => (
